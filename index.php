7b5896aa12a5662edfb2a6061d671ed4053b37ec
Ruslan-Light <73093065+Ruslan-Light@users.noreply.github.com>
2020-10-21 09:33:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+// FRONT CONTROLLER
+
+// 1. Общие настройки
+ini_set('display_errors', 1);     // вывод всех ошибок (устанавливается на время разработки)
+error_reporting(E_ALL);
+
+session_start();
+
+// 2. Подключение файлов системы
+define('ROOT', dirname(__FILE__));                   // в константу ROOT помещаем текущий каталог (__FILE__)
+require_once(ROOT.'/components/Autoload.php');
+
+// 3. Установка соединения с БД
+
+// 4. Вызов Router
+
+$router = new Router();
+$router->run();
